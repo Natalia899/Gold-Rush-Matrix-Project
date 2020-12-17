e3e3e958810db6ea33a692054193016d99e6b28e
Natalia <nataliostapiuk@gmail.com>
2020-12-17 04:34:29 +0200

    extensions1-3

--- dist/GoldRush.js
+++ dist/GoldRush.js
@@ -6,19 +6,28 @@ class GoldRush extends Matrix {
         // this.player1= {num:1, score:0}
         // this.player2= {num:2, score:0}
     }
-    loadBoard() {
-        for (let i = 0; i < 5; i++) {
+    loadBoard(rowNum, colNum) {
+        // this.matrix = this.generateMatrix(rowNum,colNum)
+        for (let i = 0; i < rowNum; i++) {
             this.matrix[i] = []
-            for (let b = 0; b < 5; b++) {
+            for (let b = 0; b < colNum; b++) {
                 this.matrix[i].push('.')
             }
-        } this.matrix[0][0] = this.players[0].num
-        this.matrix[4][4] = this.players[1].num
-        for (let c = 0; c < 10; c++) {
-            let randomRow = Math.floor(Math.random() * Math.floor(5))
-            let randomColumn = Math.floor(Math.random() * Math.floor(5))
+        }
+        this.matrix[0][0] = this.players[0].num
+        this.matrix[rowNum - 1][colNum - 1] = this.players[1].num
+        for (let c = 0; c < rowNum + 4; c++) {
+            let randomRow = Math.floor(Math.random() * Math.floor(rowNum))
+            let randomColumn = Math.floor(Math.random() * Math.floor(rowNum))
             if (this.matrix[randomRow][randomColumn] === ".") {
                 this.matrix[randomRow][randomColumn] = "c"
+            }
+        }
+        for (let w = 0; w < rowNum + 4; w++) {
+            let randomRow = Math.floor(Math.random() * Math.floor(rowNum))
+            let randomColumn = Math.floor(Math.random() * Math.floor(rowNum))
+            if (this.matrix[randomRow][randomColumn] === ".") {
+                this.matrix[randomRow][randomColumn] = "w"
             }
         }
         return this.matrix
@@ -26,32 +35,53 @@ class GoldRush extends Matrix {
     isCoin(x, y) {
         return this.matrix[x][y] === 'c' ? true : false
     }
+    isWall(x, y) {
+        return this.matrix[x][y] === 'w' ? true : false
+    }
     movePlayer(playerNum, direction) {
         let x = this.findCoordinate(playerNum).x
         let y = this.findCoordinate(playerNum).y
-        this.matrix[x][y] = '.'
+
         if (direction === 'up') {
             let relPlayer = this.players.find(p => p.num === playerNum)
+            if (this.isWall(x - 1, y)) { return alert('oops') }
+            this.matrix[x][y] = '.'
             if (this.isCoin(x - 1, y)) { relPlayer.score += 10 }
             console.log(this.players);
             this.alter(x - 1, y, playerNum)
         }
         if (direction === 'down') {
             let relPlayer = this.players.find(p => p.num === playerNum)
+            if (this.isWall(x + 1, y)) { return alert('oops') }
+            this.matrix[x][y] = '.'
             if (this.isCoin(x + 1, y)) { relPlayer.score += 10 }
             console.log(this.players);
             this.alter(x + 1, y, playerNum)
         }
         if (direction === 'left') {
             let relPlayer = this.players.find(p => p.num === playerNum)
+            if (this.isWall(x, y - 1)) { return alert('oops') }
+            this.matrix[x][y] = '.'
             if (this.isCoin(x, y - 1)) { relPlayer.score += 10 }
             this.alter(x, y - 1, playerNum)
         }
         if (direction === 'right') {
             let relPlayer = this.players.find(p => p.num === playerNum)
+            if (this.isWall(x, y + 1)) { return alert('oops') }
+            this.matrix[x][y] = '.'
             if (this.isCoin(x, y + 1)) { relPlayer.score += 10 }
             this.alter(x, y + 1, playerNum)
         }
 
     }
+    limitBounds(playerNum, direction) {
+        try {
+            this.movePlayer(playerNum, direction)
+        }
+        catch {
+            alert('no no no')
+        }
+
+    }
+
 }
--- dist/main.js
+++ dist/main.js
@@ -1,63 +1,65 @@
 const render = new Renderer()
 const board = new GoldRush()
 
-const data = board.loadBoard()
-console.log(data);
+const data = board.loadBoard(5, 5)
 render.renderBoard(data)
 render.renderScore1(board.players[0])
 render.renderScore2(board.players[1])
 
-// let scores = {player1Score: board.players[0].score,
-//              player2Score: board.players[1].score}
+$('#start').on('click', () => {
+    const rowNum = $('#rowNum').val()
+    const colNum = $('#colNum').val()
+    const data = board.loadBoard(rowNum, colNum)
+    render.renderBoard(data)
+    render.renderScore1(board.players[0])
+    render.renderScore2(board.players[1])
 
- //render.renderScores(render.renderScores(board.players[0]))
+})
 
-
-
-$(document).keypress(function (e){
-    switch(e.which) {
+$(document).keypress(function (e) {
+    switch (e.which) {
         case 119:
-            board.movePlayer(1, "up")
+            board.limitBounds(1, "up")
             render.renderBoard(data)
             //console.log(scores);
             render.renderScore1(board.players[0])
-          break;
+            break;
         case 115:
-            board.movePlayer(1, "down")
+            board.limitBounds(1, "down")
             render.renderBoard(data)
-          //  console.log(scores);
-          render.renderScore1(board.players[0])
-          break;
-          case 97:
-            board.movePlayer(1, "left")
+            //  console.log(scores);
+            render.renderScore1(board.players[0])
+            break;
+        case 97:
+            board.limitBounds(1, "left")
             render.renderBoard(data)
             render.renderScore1(board.players[0])
-          break;
-          case 100:
-            board.movePlayer(1, "right")
+            break;
+        case 100:
+            board.limitBounds(1, "right")
             render.renderBoard(data)
             render.renderScore1(board.players[0])
-          break;
-          case 105:
-            board.movePlayer(2, "up")
+            break;
+        case 105:
+            board.limitBounds(2, "up")
             render.renderBoard(data)
             render.renderScore2(board.players[1])
-          break;
+            break;
         case 107:
-            board.movePlayer(2, "down")
+            board.limitBounds(2, "down")
             render.renderBoard(data)
             render.renderScore2(board.players[1])
-          break;
-          case 106:
-            board.movePlayer(2, "left")
+            break;
+        case 106:
+            board.limitBounds(2, "left")
             render.renderBoard(data)
             render.renderScore2(board.players[1])
-          break;
-          case 108:
-            board.movePlayer(2, "right")
+            break;
+        case 108:
+            board.limitBounds(2, "right")
             render.renderBoard(data)
             render.renderScore2(board.players[1])
-          break;
-     }
+            break;
+    }
 
 })
